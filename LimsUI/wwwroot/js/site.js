f982ffd9dd178664d3ae8559a66e5d2eef58ea7c
annsj <anna4615@hotmail.com>
2022-05-02 16:45:03 +0200

    added Print Layout

--- LimsUI/wwwroot/js/site.js
+++ LimsUI/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
